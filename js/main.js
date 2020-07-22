cafb795c2dfb478711e1fc0fcdc03b9908a0df68
Rocco <rp@Roccos-iMac.local>
2020-07-22 18:31:52 -0400

    testing layout stuff

--- js/main.js
+++ js/main.js
@@ -6,6 +6,61 @@ let $titleBar = document.querySelector('.titleBar')
 let title = document.querySelector('h1').textContent
 let years = document.getElementsByClassName('year');
 
+const timeline = {
+    title: 'hello world',
+    events: [
+        {date: new Date(2001, 10, 1), text: "Blah blah"},
+        {date: new Date(2002, 10, 1), text: "Blah blah"},
+        {date: new Date(2011, 10, 1), text: "Blah blah"},
+        {date: new Date(2015, 10, 1), text: "Blah blah"},
+        {date: new Date(2026, 10, 1), text: "Blah blah"}
+    ]
+}
+
+/* const first = timeline.events[0].date
+const last = timeline.events[events.length - 1].date
+const totalDaysBetween = // difference between last and first
+
+timeline.events.forEach(e => {
+
+    const daysFromStart = // e.date - first
+
+    const percentOfLine = daysFromStart / totalDayBetween
+    const dot = document.createElement('span')
+    document.querySelector('.dynamicTimelineLine').append(dot)
+    dot.className = 'dot'
+    dot.style.setAttribute('--percent', p)
+    
+})
+ */
+
+window.addEventListener('load', event => {
+    const first = timeline.events[0].date
+    const last = timeline.events[timeline.events.length - 1].date
+    const diffTime = Math.abs(first - last)
+    const totalDaysBetween = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+    const $timeline = document.querySelector('.dynamic-timeline-line > ul')
+    $timeline.innerHTML = "" // cheating, fix this
+    // Create fragment
+
+    timeline.events.forEach(e => {
+
+        const daysFromStart = Math.ceil(Math.abs(e.date - first) / (1000 * 60 * 60 * 24))
+        const percentOfLine = daysFromStart / totalDaysBetween
+
+        const dot = document.createElement('li')
+        $timeline.append(dot)
+        dot.className = 'dynamic-timeline-line-dot'
+        dot.style.setProperty('--percent', percentOfLine)
+
+        const time = document.createElement('time')
+        dot.append(time)
+        time.className = 'purple'
+        time.setAttribute('timedate',  `${e.date.getFullYear()}-${e.date.getMonth()}-${e.date.getDate()}`)
+        time.append(e.date.getFullYear())
+    })
+})
+
 window.addEventListener('scroll', event => {
     let windowH = window.innerHeight
     let documentH = document.documentElement.scrollHeight
@@ -36,3 +91,8 @@ window.addEventListener('scroll', event => {
         years[i].style.backgroundColor = "red"
     }
 
+
+
+
+
+    
