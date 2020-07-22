b47d45e27aa112ed39828b7c00fcf9f47aa80bf5
Antonio Valdivia <antoniovaldivia@gmail.com>
2020-07-22 13:34:08 -0400

    main.js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let $progress = document.querySelector('.progress')
+let $h1 = document.getElementById('heading')
+let $titleBar = document.querySelector('.titleBar')
+let title = document.querySelector('h1').textContent
+
+window.addEventListener('scroll', event => {
+    let windowH = window.innerHeight
+    let documentH = document.documentElement.scrollHeight
+    let amtScrolled = window.scrollY
+    let ttlAvailable = documentH - windowH
+    let percent = amtScrolled / ttlAvailable
+    $progress.style.width = `${percent * 100}%`
+
+    let h1Top = $h1.offsetTop
+    let h1Height = $h1.clientHeight
+    console.log(h1Top, h1Height, amtScrolled)
+
+    if (amtScrolled > h1Top + h1Height) {
+        console.log('H1 is off the top')
+        $titleBar.classList.add(`show`)
+        $progress.classList.remove(`hide`)
+        $titleBar.innerHTML = `${title}`;
+
+    } else {
+        $titleBar.classList.remove(`show`)
+        $progress.classList.add(`hide`)
+        $titleBar.innerHTML = ``;
+    }
+
+})
